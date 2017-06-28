9f092dfd74ebc1cd645a6a58f293425b6c04f89e
Degiovanni Emilio <emiliodeg@gmail.com>
2017-06-27 21:27:46 -0300

    Update ImageResize.php
    I made a little mistake, now quality is ok

--- lib/ImageResize.php
+++ lib/ImageResize.php
@@ -147,7 +147,7 @@ class ImageResize
     public function save($filename, $image_type = null, $quality = null, $permissions = null)
     {
         $image_type = $image_type ?: $this->source_type;
-        $quality = (int) abs($quality);
+        $quality = is_numeric($quality) ? (int) abs($quality) : null;
 
         switch ($image_type) {
             case IMAGETYPE_GIF:
@@ -203,7 +203,7 @@ class ImageResize
                 break;
 
             case IMAGETYPE_JPEG:
-                if ($quality === null || $quality < 0 || $quality > 100) {
+                if ($quality === null || $quality > 100) {
                     $quality = $this->quality_jpg;
                 }
 
@@ -211,7 +211,7 @@ class ImageResize
                 break;
 
             case IMAGETYPE_PNG:
-                if ($quality === null || $quality < 0 || $quality > 9) {
+                if ($quality === null || $quality > 9) {
                     $quality = $this->quality_png;
                 }
 
